edc9e192e8b03d97f35a7fe6486ce2c88c801a6b
Magicrafter13 <scubaventure101@hotmail.com>
2017-10-16 10:11:16 -0700

    I own version of a revert. Because I can't figure out how git works so I just copied the code from an older commit. Jesus this should not be this complicated.

--- Breakout/source/Breakout.cpp
+++ Breakout/source/Breakout.cpp
@@ -6,7 +6,7 @@
 
 //init
 char versiontxtt[8] = "  Beta ", versiontxtn[9] = "01.05.00";
-char buildnumber[14] = "17.10.13.2330", ishupeversion[9] = "00.03.01";
+char buildnumber[14] = "17.10.15.1213", ishupeversion[9] = "00.03.01";
 int vernumqik = 0;
 u32 kDown, kHeld;
 
@@ -20,12 +20,12 @@ double ball_dx, ball_dy, ball_angle, trail_new_frame_x[8], trail_new_frame_y[8];
 int last_power, times_power_1, times_power_2, times_power_3;
 int press_select_frame = 0; bool press_select_visible = true;
 
-paddle the_paddle; ball the_ball; mCircle trail_new_frame_circle[8]; brick brick_array[3][50];
+paddle the_paddle; ball the_ball; mCircle trail_new_frame_circle[8]; brick brick_array[2][50];
 sf2d_texture *img_thanksbeta, *img_paddle, *img_brick00, *img_brick01, *img_brick02, *img_brick03, *img_brick04, *img_brick05, *img_waveform;
 SFX_s *testsound[1], *ball_bounce[8];
 
 /*integer mask for levels*/
-int level_mask[3][50] = {
+int level_mask[2][50] = {
 	{
 		4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
 		3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
@@ -34,18 +34,11 @@ int level_mask[3][50] = {
 		0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 	},
 	{
-		4, 4, brick_types, brick_types, brick_types, brick_types, brick_types, brick_types, brick_types, brick_types,
-		3, 3, 3, 3, brick_types, brick_types, brick_types, brick_types, brick_types, brick_types,
-		2, 2, 2, 2, 2, 2, brick_types, brick_types, brick_types, brick_types,
-		1, 1, 1, 1, 1, 1, 1, 1, brick_types, brick_types,
+		4, 4, max_textures, max_textures, max_textures, max_textures, max_textures, max_textures, max_textures, max_textures,
+		3, 3, 3, 3, max_textures, max_textures, max_textures, max_textures, max_textures, max_textures,
+		2, 2, 2, 2, 2, 2, max_textures, max_textures, max_textures, max_textures,
+		1, 1, 1, 1, 1, 1, 1, 1, max_textures, max_textures,
 		0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-	},
-	{
-		brick_types, brick_types, 0, 0, 1, 1, 0, 0, brick_types, brick_types,
-		          0,           0, 1, 1, 2, 2, 1, 1,           0,           0,
-				  1,           1, 2, 2, 8, 8, 2, 2,           1,           1,
-				  0,           0, 1, 1, 2, 2, 1, 1,           0,           0,
-		brick_types, brick_types, 0, 0, 1, 1, 0, 0, brick_types, brick_types,
 	}
 };
 
@@ -166,22 +159,11 @@ int main(int argc, char **argv)
 	{
 		for (int b = 0; b < 10; b++)
 		{
-			if (level_mask[1][array_step] == brick_types)
+			if (level_mask[1][array_step] == max_textures)
 				brick_array[1][array_step].setDefaults((40 * b) + 2, ((20 * a) + 2), 36, 16, a, false, 0);
 			else
-				brick_array[1][array_step].setDefaults((40 * b) + 2, ((20 * a) + 2), 36, 16, a, true, level_mask[1][array_step]);
+				brick_array[1][array_step].setDefaults((40 * b) + 2, ((20 * a) + 2), 36, 16, a, true, level_mask[0][array_step]);
 			array_step++;
-		}
-	}
-	/*brick array level 3, 5 times by 10 times (10 bricks across, 5 down)*/
-	for (int a = 0; a < 5; a++)
-	{
-		for (int b = 0; b < 10; b++)
-		{
-			if (level_mask[2][array_step] == brick_types)
-				brick_array[2][array_step].setDefaults((40 * b) + 2, ((20 * a) + 2), 36, 16, a, false, 0);
-			else
-				brick_array[2][array_step].setDefaults((40 * b) + 2, ((20 * a) + 2), 36, 16, a, true, level_mask[2][array_step]);
 		}
 	}
 
@@ -316,10 +298,10 @@ int breakout()
 	hidScanInput();
 	kDown = hidKeysDown();
 	kHeld = hidKeysHeld();
-	if (kDown & KEY_R)
+	/*if (kDown & KEY_R)
 		level++;
 	if (kDown & KEY_L)
-		level--;
+		level--;*/
 	if (kDown & KEY_SELECT || lives == 0) return 2;
 	if (kHeld & KEY_START) return 3;
 	/*move paddle left (if applicable)*/
@@ -395,18 +377,24 @@ int breakout()
 					if (the_ball.getBottom(true) >= the_paddle.paddle_mrect.x + (paddle_width_ninth * z))
 						angle += 1;
 				/*change ball angle according to the area of paddle hit*/
-				switch (angle)
-				{
-					case 1: ball_angle = (360.0 - ball_angle) - 40.0;
-					case 2: ball_angle = (360.0 - ball_angle) - 30.0;
-					case 3: ball_angle = (360.0 - ball_angle) - 20.0;
-					case 4: ball_angle = (360.0 - ball_angle) - 10.0;
-					case 5: ball_angle = (360.0 - ball_angle);
-					case 6: ball_angle = (360.0 - ball_angle) + 10.0;
-					case 7: ball_angle = (360.0 - ball_angle) + 20.0;
-					case 8: ball_angle = (360.0 - ball_angle) + 30.0;
-					case 9: ball_angle = (360.0 - ball_angle) + 40.0;
-				}
+				if (angle == 1)
+					ball_angle = (360.0 - ball_angle) - 40.0;
+				if (angle == 2)
+					ball_angle = (360.0 - ball_angle) - 30.0;
+				if (angle == 3)
+					ball_angle = (360.0 - ball_angle) - 20.0;
+				if (angle == 4)
+					ball_angle = (360.0 - ball_angle) - 10.0;
+				if (angle == 5)
+					ball_angle = (360.0 - ball_angle);
+				if (angle == 6)
+					ball_angle = (360.0 - ball_angle) + 10.0;
+				if (angle == 7)
+					ball_angle = (360.0 - ball_angle) + 20.0;
+				if (angle == 8)
+					ball_angle = (360.0 - ball_angle) + 30.0;
+				if (angle == 9)
+					ball_angle = (360.0 - ball_angle) + 40.0;
 			}
 			/*large if statement to determine if a brick has been hit (run once per brick)*/
 			for (int j = 0; j < 50; j++)
--- Breakout/source/Breakout.hpp
+++ Breakout/source/Breakout.hpp
@@ -32,10 +32,6 @@ extern PrintConsole topScreen, bottomScreen, versionWin, killBox, debugBox;
 #include "brick04_png.h"
 #include "brick05_png.h"
 #include "ball00_png.h"
-#include "brick06_png.h"
-#include "brick07_png.h"
-#include "brick08_png.h"
-#include "brick09_png.h"
 
 #include "ethnocen_ttf.h"
 
